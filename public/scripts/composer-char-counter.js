4ea58aa841cdf83b99141a857301a6cc903e4079
Alefiya <alefiyavahanvaty@gmail.com>
2018-09-12 01:31:27 +0000

    added changing colour functionaity when chars are negative

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,16 @@ $(document).ready(function() {
 
 let textarea = $('textarea');
 
-textarea.on('keypress', function() {
-  let originalCount = 139;
-  let counted = $(this).val().length;
-  console.log(originalCount - counted)
+textarea.on('input', function() {
+  const counted = $(this).val().length;
+  const counter = $(this).siblings('.counter');
+  const charactersRemaining = 140 - counted;
+  const updatedCount = counter.text(charactersRemaining);
+  if (updatedCount.text() < 1) {
+  updatedCount.css("color", "red");
+  } else {
+    updatedCount.css("color", "black");
+  }
 });
 
 
