0f72c473d9388cbb877d75c6f9741fcf6a5a888b
Alefiya <alefiyavahanvaty@gmail.com>
2018-09-13 03:04:30 +0000

    added conditionals so that user cannot enter empty string or tweet over 140. form validation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,53 +1,4 @@
 $(document).ready(function() {
-  alert("hello");
-
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
-
 
   function renderTweets(tweets) {
     for (content in tweets) {
@@ -79,7 +30,7 @@ $(document).ready(function() {
   };
 
 
-//$.ajax('/tweets').done(renderTweets);
+$.ajax('/tweets').done(renderTweets);
 
 function callTweetsFromMongoDb(){
 
@@ -87,7 +38,6 @@ function callTweetsFromMongoDb(){
       url: '/tweets',
       method: 'GET',
       success: function(result){
-        console.log("result ",result);
         console.log("we are in success");
         renderTweets(result);
       },
@@ -102,7 +52,13 @@ $('#new-tweet-form').on('submit', function (e) {
   e.preventDefault();
 
   let formData = $('#new-tweet-form').serialize();
-  console.log(formData);
+  let text = $('#tweet-text').val()
+
+  if (text === "" || text === null || text.length > 140) {
+    alert("Please enter a valid tweet");
+    return;
+  }
+  //console.log(formData);
   $.ajax({
     method:'POST',
     url: '/tweets',
